1dbaebfb9889400f4ac17d7ffaae83841e883d65
Till Zickmantel <till.zickmantel@gmail.com>
2021-12-05 14:26:29 +0100

    ADD: Puzzle 2.1 moving the sub

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod puzzle_1;
+mod puzzle_2;
 
 fn main() {
     println!("Puzzle 1.1: There are {} instances of increased depth.", 
              puzzle_1::count_increasing_depth());
     println!("Puzzle 1.2: There are {} instances of increased depth using a sliding window of size 3.", 
              puzzle_1::count_sliding_window_increasing_depth());
+    println!("Puzzle 2.1: The final posision metric of the sub (horizontal x depth) is {}.",
+             puzzle_2::compute_position());
 }
---
+++ src/puzzle_2.rs
@@ -0,0 +1,91 @@
+use std::str::FromStr;
+
+const INPUT: &str = include_str!("../data/2/input");
+
+#[derive(Debug)]
+enum Direction 
+{
+    UP, 
+    DOWN, 
+    FORWARD,
+}
+
+#[derive(Debug)]
+struct Move 
+{
+    direction: Direction,
+    stepsize: i32,
+}
+
+#[derive(Debug)]
+struct Position
+{
+    horizontal: i32,
+    depth: i32,
+}
+
+impl Position {
+    fn new() -> Self
+    {
+        Position{horizontal: 0, depth: 0}
+    }
+
+    fn update(self: &mut Self, m: Move) -> () 
+    {
+        use Direction::*;
+        match m.direction {
+            UP => {self.depth -= m.stepsize}
+            DOWN => {self.depth += m.stepsize}
+            FORWARD => {self.horizontal += m.stepsize}
+        }
+    }
+
+    fn metric(self: &Self) -> i32
+    {
+        self.horizontal * self.depth
+    }
+}
+
+struct ParseDirectionError {}
+
+impl FromStr for Direction {
+    type Err = ParseDirectionError;
+    fn from_str(s: &str) -> Result<Self, Self::Err>
+    {
+        match s {
+            "up" => Ok(Direction::UP),
+            "down" => Ok(Direction::DOWN),
+            "forward" => Ok(Direction::FORWARD),
+            _ => Err(Self::Err{})
+        }
+    }
+}
+
+fn parse_move(s: &str) -> Option<Move>
+{
+    let mut it = s.split(' ');
+    Some(
+        Move {
+           direction: it.next()?.parse().ok()?,
+           stepsize: it.next()?.parse().ok()?,
+        }
+    )
+}
+
+fn parse_input(s: &str) -> Vec<Move>
+{
+    s
+        .split('\n')
+        .filter_map(parse_move)
+        .collect()
+}
+
+pub fn compute_position() -> i32
+{
+    let data = parse_input(INPUT);
+    let mut position = Position::new();
+    for m in data {
+        position.update(m);
+    }
+    position.metric()
+}
